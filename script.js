8e26615370d3fe42c0e8dff5d455481c3cef1e95
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-09-19 18:31:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here
+const guessedNo = document.getElementById("guess");
+const btn = document.getElementById("submit");
+const respond = document.getElementById("respond");
+
+const secretNo = Math.floor(Math.random()*100);
+
+btn.onclick = () => {
+	if(parseInt(guessedNo.value) < secretNo){
+		respond.textContent = "guess higher";
+	} else if(parseInt(guessedNo.value) > secretNo) {
+		respond.textContent = "guess lower";
+	} else {
+		respond.textContent = "correct number";
+	}
+}
+
+
+
 
